07e964d98a4bf62766a0506cf24f478caf44a9f7
Niraj chaudhari <151720495+Niraj98-stack@users.noreply.github.com>
2024-11-30 20:07:14 +0530

    Update CSSStub.js

--- config/CSSStub.js
+++ config/CSSStub.js
@@ -1,1 +1,21 @@
-module.exports = {};
+import React, { useState, useEffect } from 'react';
+import WeatherDisplay from './WeatherDisplay';
+
+const App = () => {
+  const [weather, setWeather] = useState({ temperature: 0, conditions: '' });
+
+  useEffect(() => {
+    // Simulate fetching weather data
+    const weatherInput = { temperature: 25, conditions: 'Sunny' };
+    setWeather(weatherInput);
+  }, []);
+
+  return (
+    <div>
+      <h1>Weather Report</h1>
+      <WeatherDisplay weather={weather} />
+    </div>
+  );
+};
+
+export default App;
